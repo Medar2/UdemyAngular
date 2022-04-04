46297d613c13e61bcce4473b4783a56b95539ae0
Jose Caraballo <jcaraballo74@hotmail.com>
2022-04-04 14:03:23 -0400

    [ADD] Configuraciín de los Ingresos

--- presupuesto-app/src/app/ingreso/ingreso.servicio.ts
+++ presupuesto-app/src/app/ingreso/ingreso.servicio.ts
@@ -5,4 +5,9 @@ export class IngresoServicio{
         new Ingreso("Salario", 4000),
         new Ingreso("Venta de coche", 500)
     ];
+
+    eliminarRegistro(ingreso: Ingreso){
+        const indice: number = this.ingresos.indexOf(ingreso);
+        this.ingresos.splice(indice, 1); //elimina el elemento del array, el primer parametro es el indice, el segundo es el numero de elementos a eliminar
+    }
 }
