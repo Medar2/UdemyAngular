57d93625f4f92358549e1fa95b7cc8a4b734713d
Jose Caraballo <jcaraballo74@hotmail.com>
2022-04-07 21:52:32 -0400

    agregar Obtener personas y EditarPersona

--- listado-personas/src/app/data.services.ts
+++ listado-personas/src/app/data.services.ts
@@ -5,14 +5,28 @@ import { Persona } from "./persona.model";
 
 @Injectable()
 export class DataServices {
+    baseUrl: string = 'https://listadopersonas-5e885-default-rtdb.firebaseio.com/datos.json';
     constructor(private httpClient: HttpClient){}
 
     guardarPersona(personas: Persona[]) {
-        this.httpClient.put('https://listadopersonas-5e885-default-rtdb.firebaseio.com/datos.json',personas)
+        this.httpClient.put(this.baseUrl,personas)
         .subscribe ( response => {
             console.log("resultado guardar personas :" + response);
          },
                  error => console.log("Error al guardar persona" + error)  
         );
+    }
+
+    cargarPersonas (){
+        return this.httpClient.get(this.baseUrl);
+    }
+    modificarPersona(index:number, persona: Persona){
+        let url: string;
+         url = this.baseUrl.replace('.json', '/' + index + '.json');
+         this.httpClient.put(url, persona)
+         .subscribe(
+             response => console.log("resultado modificar persona :" + response),
+                 error => console.log("Error al modificar persona" + error)
+         );
     }
 }
--- listado-personas/src/app/persona.service.ts
+++ listado-personas/src/app/persona.service.ts
@@ -5,17 +5,21 @@ import { Persona } from './persona.model';
 
 @Injectable()
 export class PersonasService {
-  personas: Persona[] = [
-    new Persona('Juan', 'Perez'),
-    new Persona('Laura', 'Juarez'),
-  ]; 
+  personas: Persona[] = []; 
 
   saludar = new EventEmitter<number>();
 
   constructor(private loggingService:LoggingService, private dataServices: DataServices){}
 
+  setPersonas(personas: Persona[]){
+    this.personas = personas;
+  }
+
   agregarPersona(persona: Persona) {
     this.loggingService.enviaMensajeAConsola('agregamos persona:' + persona.nombre)
+    if(this.personas == null){
+      this.personas = [];
+    }
     this.personas.push(persona);
     this.dataServices.guardarPersona(this.personas);
   }
@@ -29,9 +33,14 @@ export class PersonasService {
     let persona1 = this.personas[index];
     persona1.nombre = persona.nombre;
     persona1.apellido = persona.apellido;
+    this.dataServices.modificarPersona(index, persona);
   }
   
   eliminarPersona(index:number){
     this.personas.splice(index, 1);
+  }
+  obtenerPersonas () {
+    return this.dataServices.cargarPersonas();
+
   }
 }
