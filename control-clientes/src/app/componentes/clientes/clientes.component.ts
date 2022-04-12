f0150dadcd7fb3b1911cfa5c2572bf8af16fb0a4
Jose Caraballo <jcaraballo74@hotmail.com>
2022-04-12 18:01:15 -0400

    [add] Add vali form

--- control-clientes/src/app/componentes/clientes/clientes.component.ts
+++ control-clientes/src/app/componentes/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'flash-messages-angular';
 import { Clientes } from 'src/app/modelo/cliente.model';
 import { ClienteServicio } from 'src/app/servicios/cliente.service';
 
@@ -9,10 +10,18 @@ import { ClienteServicio } from 'src/app/servicios/cliente.service';
 })
 export class ClientesComponent implements OnInit {
   clientes: Clientes[];
+  cliente: Clientes = {
+    nombre: '',
+    apellido: '',
+    email: '',
+    saldo: 0
+  };
 
-  constructor(private clienteServio: ClienteServicio) { 
+  constructor(private clienteServio: ClienteServicio,clienteServicio: ClienteServicio, private flashMessages: FlashMessagesService) { 
     this.clientes =[];
   }
+    
+  
   
   
   ngOnInit(): void {
@@ -30,6 +39,15 @@ export class ClientesComponent implements OnInit {
       })
     }
     return saldoTotal;
+  }
+  agregar({value, valid}: {value: Clientes, valid: boolean}){
+    if(!valid){
+      this.flashMessages.show('Por favor llena el formulario correctamente', {  cssClass: 'alert-danger', timeout: 4000});
+    }else{
+      
+
+     //this.clienteServio.agregarCliente(value);
+    }
   }
 
 }
