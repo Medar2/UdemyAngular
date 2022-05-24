65a0413cb66248f7ecac5181774eee3831e25493
Medard2 <jcaraballo74@hotmail.com>
2022-05-24 00:00:09 -0400

    [FIX] login servicest

---
+++ control-clientes/src/app/servicios/login.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/compat/auth";
+
+//No olvidar registar en el app.module.ts en providers: [LoginService]
+@Injectable() // Decorador para indicar que esta clase es un servicio y para poder inyectarla en otros componentes
+export class LoginService {
+
+    constructor(private authService: AngularFireAuth) { }
+
+    login(email: string, password: string) {    
+        return new Promise((resolve, reject) => {
+            this.authService.signInWithEmailAndPassword(email, password).then(userData => resolve(userData),
+                err => reject(err));
+        });
+    }
+
+    
+}
